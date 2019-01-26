14daf0d18535ef26b54137e1e90e91cc4d2b1a01
Bertina Zhai <bertina.han@gmail.com>
2019-01-26 13:35:19 -0800

    Add test for card model

--- Library/Model/Card.cs
+++ Library/Model/Card.cs
@@ -5,19 +5,39 @@ namespace Library.Model
 {
     public class Card : IComparable<Card>
     {
-        public CardRank Rank { get; private set; }
-        public CardSuit Suit { get; private set; }
+        public CardRank Rank { get; }
+        public CardSuit Suit { get; }
+
+        /**
+         * Card model
+         * 
+         * @param {CardRank} rank rank of the card
+         * @param {CardSuit} suit suit of the card
+         */
         public Card(CardRank rank, CardSuit suit)
         {
             Rank = rank;
             Suit = suit;
         }
 
+        /**
+         * Compare card with other card
+         * 
+         * @param  {Card}     otherCard card to compare
+         * @return {Integer}            compare result
+         */
         public int CompareTo(Card otherCard)
         {
-            return Rank - otherCard.Rank;
+            if (otherCard == null) return 1;
+
+            return Rank.CompareTo(otherCard.Rank);
         }
 
+        /**
+         * Convert card to string
+         * 
+         * @return {string} card rank and suit string
+         */
         public override string ToString()
         {
             return Rank.ToString() + " " + Suit.ToString();
---
+++ Test/Library/Model/CardTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using Library.Model;
+using Library.Utilities;
+
+namespace Test.Library.Model
+{
+    [TestFixture()]
+    public class CardTest
+    {
+        [Test()]
+        public void Test_Card()
+        {
+            Card card = new Card(CardRank.Ace, CardSuit.CLUB);
+            Assert.IsTrue(card.Rank == CardRank.Ace);
+            Assert.IsTrue(card.Suit == CardSuit.CLUB);
+        }
+
+        [Test()]
+        public void Test_CompareTo()
+        {
+            Card cardA = new Card(CardRank.Ace, CardSuit.CLUB);
+            Card cardB = new Card(CardRank.Eight, CardSuit.DIAMOND);
+            Assert.IsTrue(cardA.CompareTo(cardB) == 1);
+            Assert.IsTrue(cardA.CompareTo(null) == 1);
+            Assert.IsTrue(cardA.CompareTo(cardA) == 0);
+            Assert.IsTrue(cardB.CompareTo(cardA) == -1);
+        }
+
+        [Test()]
+        public void Test_ToString()
+        {
+            Card card = new Card(CardRank.Ace, CardSuit.CLUB);
+            Assert.IsTrue(card.ToString().Equals("Ace CLUB"));
+        }
+    }
+}
